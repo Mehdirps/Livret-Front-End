c3631e8f3af700e8fbabc91cf9b7c8ccd2585e2f
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-06 17:36:11 +0200

    UP : Stats Card component

---
+++ src/components/dashboard/StatsCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const StatsCard = ({data, bgColor, title}) => {
+    return (
+        <div className="col-lg-3 col-md-4 col-sm-6">
+            <div className={`card text-white bg-${bgColor} mb-3`}>
+                <div className="card-header">{title}</div>
+                <div className="card-body">
+                    <h5 className="card-title">{data}</h5>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StatsCard;
