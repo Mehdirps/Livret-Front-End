4c43539d4e3ab6f6499c4588eed1cdc5eb40f0d7
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-08 21:23:34 +0200

    UP : Enable/Disable suggestion

---
+++ src/components/dashboard/SuggestionStatusToggle.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {setLivret} from "../../stores/slices/livretSlice";
+
+const SuggestionStatusToggle = ({livret}) => {
+
+    const token = useSelector(state => state.user.token);
+    const dispatch = useDispatch();
+
+    const handleToggle = () => {
+        fetch(`${process.env.REACT_APP_API_URL}dashboard/suggestion/enable/${livret.id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                dispatch(setLivret({livret: data.livret}));
+            })
+            .catch(error => console.error(error));
+    }
+
+    return (
+        <div>
+            {!livret.suggest ? (
+                <div>
+                    <div className="alert alert-warning" role="alert">
+                        Les suggestions ne sont pas activées sur votre livret
+                    </div>
+                    <div className="btn btn-primary" onClick={() => handleToggle()}>
+                        Activer les suggestions
+                    </div>
+
+                </div>
+            ) : (
+                <div>
+                    <div className="alert alert-success" role="alert">
+                        Les suggestions sont activées sur votre livret
+                    </div>
+                    <div className="btn btn-danger" onClick={() => handleToggle()}>
+                        Désactiver les suggestions
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SuggestionStatusToggle;
