299605c99e5ed83ce05645be656db8772bf28fb3
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-09 10:39:11 +0200

    UP : Export suggestion to PDF component

---
+++ src/components/dashboard/ExportPdfButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+import {useSelector} from "react-redux";
+
+const ExportPdfButton = ({suggestions}) => {
+
+    const livret = useSelector(state => state.livret.livret);
+    const handleExportPdf = () => {
+        const doc = new jsPDF();
+
+        doc.text(`Suggestion du livret "${livret.livret_name}"`, 15, 10);
+
+        doc.autoTable({
+            startY: 20,
+            head: [['Nom','Email','Titre', 'Message', 'Statut']],
+            body: suggestions.map(s => [s.name, s.email, s.title, s.message, s.status]),
+        });
+
+        doc.save('suggestions.pdf');
+    };
+
+    return (
+        <button className="btn btn-primary" onClick={() => handleExportPdf()}>
+            Exporter en PDF
+        </button>
+    );
+};
+
+export default ExportPdfButton;
