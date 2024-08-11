a577ef56909b7da88c5440136ddd1bfafbc0f0eb
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-11 17:00:35 +0200

    UP : Inventories route & page

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IndexLayout from "../../layouts/IndexLayout";
 import Welcome from "../../pages/Welcome";
 import Authentification from "../../pages/Authentification";
@@ -11,23 +11,25 @@ import Livret from "../../pages/Livret";
 import Background from "../../pages/dashboard/Background";
 import Stats from "../../pages/dashboard/Stats";
 import Suggest from "../../pages/dashboard/Suggest";
+import Inventories from '../../pages/dashboard/Inventories';
 
 const Index = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={'/livret/:slug/:id'} element={<Livret/>}/>
-                <Route element={<IndexLayout/>}>
-                    <Route index element={<Welcome/>}/>
-                    <Route path={'/connexion'} element={<Authentification/>}/>
+                <Route path={'/livret/:slug/:id'} element={<Livret />} />
+                <Route element={<IndexLayout />}>
+                    <Route index element={<Welcome />} />
+                    <Route path={'/connexion'} element={<Authentification />} />
                 </Route>
-                <Route path={'/dashboard'} element={<DashboardLayout/>}>
-                    <Route path={'premiere_connexion'} element={<FirstLogin/>}/>
-                    <Route index element={<DashboardIndex/>}/>
-                    <Route path={'profil'} element={<Profile/>}/>
-                    <Route path={'background'} element={<Background/>}/>
-                    <Route path={'stats'} element={<Stats/>}/>
-                    <Route path={'suggestions'} element={<Suggest/>}/>
+                <Route path={'/dashboard'} element={<DashboardLayout />}>
+                    <Route path={'premiere_connexion'} element={<FirstLogin />} />
+                    <Route index element={<DashboardIndex />} />
+                    <Route path={'profil'} element={<Profile />} />
+                    <Route path={'background'} element={<Background />} />
+                    <Route path={'stats'} element={<Stats />} />
+                    <Route path={'suggestions'} element={<Suggest />} />
+                    <Route path={'inventories'} element={<Inventories />} />
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ src/pages/dashboard/Inventories.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import InventoriesSearchForm from '../../components/dashboard/inventories/InventoriesSearchForm';
+import InventoriesTable from '../../components/dashboard/inventories/InventoriesTable';
+import InventoriesFilter from '../../components/dashboard/inventories/InventoriesFilter';
+import InventoriesExportPDF from '../../components/dashboard/inventories/InventoriesExportPDF';
+
+const Inventories = () => {
+    const token = useSelector(state => state.user.token);
+    const [inventories, setInventories] = React.useState([]);
+    const [filteredInventories, setFilteredInventories] = React.useState([]);
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + 'dashboard/inventories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setInventories(data.inventories);
+                setFilteredInventories(data.inventories);
+            })
+            .catch(err => console.log(err));
+    }, [token]);
+
+
+    return (
+        <div className="container">
+            <h2 className="mb-4">Etats de lieux</h2>
+            <InventoriesSearchForm />
+            <hr />
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addInventoryModal">
+                Ajouter un état des lieux
+            </button>
+            <hr />
+            <p>Exporter en PDF les états des lieux affichées dans le tableau</p>
+            <InventoriesExportPDF />
+            <hr />
+            <InventoriesFilter inventories={inventories} setFilteredInventories={setFilteredInventories} />
+            <InventoriesTable inventories={filteredInventories} />
+        </div>
+    );
+};
+
+export default Inventories;
