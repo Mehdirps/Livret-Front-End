2b8b85998f288275804605bb604cd356c06a53aa
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-15 15:44:05 +0200

    FIX: close error & success modal

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ErrorModal = ({ error }) => {
+const Error = ({ error }) => {
+
+    const [show, setShow] = useState(true);
+
     return (
-        <div className="modal d-block" role="dialog" style={{ display: 'block' }}>
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Une erreur est survenue</h5>
-                        <button type="button" className="btn-close" aria-label="Close" data-dismiss="modal"></button>
-                    </div>
-                    <div className="modal-body">
-                        <div className="alert alert-danger" role="alert">
-                            {error}
+        <>
+            {
+                show &&
+                <div className="modal d-block" role="dialog" style={{ display: 'block' }}>
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Erreur</h5>
+                                <button type="button" className="btn-close" aria-label="Close" onClick={() => setShow(false)}></button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="alert alert-danger" role="alert">
+                                    {error}
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" onClick={() => setShow(false)}>Fermer</button>
+                            </div>
                         </div>
                     </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                    </div>
                 </div>
-            </div>
-        </div>
+            }
+        </>
     );
 };
 
-export default ErrorModal;
+export default Error;
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Success = ({ success }) => {
+
+    const [show, setShow] = useState(true);
+
     return (
-        <div className="modal d-block" role="dialog" style={{ display: 'block' }}>
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Succès</h5>
-                        <button type="button" className="btn-close" aria-label="Close" data-bs-dismiss="modal"></button>
-                    </div>
-                    <div className="modal-body">
-                        <div className="alert alert-success" role="alert">
-                            {success}
+        <>
+            {
+                show &&
+                <div className="modal d-block" role="dialog" style={{ display: 'block' }}>
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Succès</h5>
+                                <button type="button" className="btn-close" aria-label="Close" onClick={() => setShow(false)}></button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="alert alert-success" role="alert">
+                                    {success}
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" onClick={() => setShow(false)}>Fermer</button>
+                            </div>
                         </div>
                     </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                    </div>
                 </div>
-            </div>
-        </div>
+            }
+        </>
     );
 };
 
