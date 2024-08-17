fd5f9aaafed5ae5282b7379e777c8338af81424d
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-17 19:51:45 +0200

    UP : change product quantity component & function

---
+++ src/components/dashboard/cart/ChangeProductQuantity.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCart } from '../../../stores/slices/cartSlice';
+
+const ChangeProductQuantity = ({ product }) => {
+    const cart = useSelector(state => state.cart.cart);
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+        const newQuantity = parseInt(e.target.value);
+
+        const newCart = cart.map(item => {
+            if (item.id === product.id) {
+                return { ...item, quantity: newQuantity };
+            }
+            return item;
+        });
+
+        dispatch(setCart({ cart: newCart }));
+    }
+
+    return (
+        <input 
+            style={{ width: '70px' }} 
+            className='form-control' 
+            type="number" 
+            value={product.quantity}
+            onChange={(e) => {
+                if (e.target.value > 0) {
+                    handleChange(e);
+                }
+            }} 
+        />
+    );
+};
+
+export default ChangeProductQuantity;
