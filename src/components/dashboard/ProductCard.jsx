2a0b7c26949f610bb0d592b0bf2ef0197e5e364d
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-17 18:33:39 +0200

    UP : AddToCart function

--- src/components/dashboard/ProductCard.jsx
+++ src/components/dashboard/ProductCard.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
 const ProductCard = ({ product }) => {
+
+    const addToCart = (product) => {
+        if (localStorage.getItem('cart') === null) {
+            localStorage.setItem('cart', JSON.stringify([]));
+        }
+
+        const cart = JSON.parse(localStorage.getItem('cart'));
+
+        product.quantity = 1;
+
+        if (cart.find(p => p.id === product.id)) {
+            cart.find(p => p.id === product.id).quantity++;
+        } else {
+            cart.push(product);
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }
+
     return (
         <div className="col-md-4 col-6">
             <div className="card h-100">
@@ -13,7 +32,9 @@ const ProductCard = ({ product }) => {
                     <h5 className="card-title">{product.name}</h5>
                     <p className="card-text">{product.description}</p>
                     <p className="card-text">{product.price} €</p>
-                    <a href={product.url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Voir</a>
+                    <div className="btn btn-primary" onClick={() => {
+                        addToCart(product);
+                    }}>Ajouter</div>
                 </div>
             </div>
         </div>
