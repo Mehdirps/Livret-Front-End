782ac38c76ca250a2ea9844c70da926feaed2d62
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-17 19:13:48 +0200

    UP : cart store for set & clear cart

---
+++ src/stores/slices/cartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cart: [],
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        setCart: (state, action) => {
+            state.cart = action.payload.cart;
+            localStorage.setItem('cart', JSON.stringify(action.payload.cart));
+        },
+        clearCart: (state) => {
+            state.cart = [];
+            localStorage.removeItem('cart');
+        }
+    },
+});
+
+export const { setCart, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
--- src/stores/stores.js
+++ src/stores/stores.js
@@ -2,11 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import livretReducer from './slices/livretSlice';
+import cartReducer from './slices/cartSlice';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         livret : livretReducer,
+        cart: cartReducer,
+        
     },
 });
 
