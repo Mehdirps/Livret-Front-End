a028216770ef34916594c17ea45f2717d0171a2a
Mehdirps <mehdi.raposo77@gmail.com>
2024-08-19 15:59:55 +0200

    UP : orders page

---
+++ src/pages/dashboard/Order.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import OrdersTable from '../../components/dashboard/OrdersTable';
+
+const Order = () => {
+    const token = useSelector(state => state.user.token);
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + 'dashboard/userOrders', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                setOrders(data.orders);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }, [token]);
+
+    return (
+        <div className="container mt-5">
+            <h1>Liste des Commandes</h1>
+        </div>
+    );
+};
+
+export default Order;
