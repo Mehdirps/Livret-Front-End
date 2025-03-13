41ef7e4c4fe87ef293be87135e4d03c1033a87bc
Mehdirps <mehdi.raposo77@gmail.com>
2025-03-13 17:12:47 +0100

    🎨 cart quanity

--- src/components/dashboard/shop/cart/Cart.jsx
+++ src/components/dashboard/shop/cart/Cart.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import CartModal from './CartModal';
 import PaiementsModal from './PaiementsModal';
 
 const Cart = () => {
     const cart = useSelector(state => state.cart.cart);
+
+    const [cartLength, setCartLength] = useState(cart.length);
+
+    useEffect(() => {
+        let length = 0;
+        cart.forEach(item => {
+            length += item.quantity;
+        });
+
+        setCartLength(length);
+    }
+    , [cart]);
     
     const calculateTotal = () => {
         return cart.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
@@ -12,9 +24,9 @@ const Cart = () => {
 
     return (
         <>
-            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cartModal">
+            <button type="button" className="btn btn-primary cart-button" data-bs-toggle="modal" data-bs-target="#cartModal">
                 <i className="bi bi-cart-fill"></i>
-                Mon panier
+                Mon panier <span className='cart_quantity'>{cartLength}</span>
             </button>
             <PaiementsModal calculateTotal={calculateTotal} />
             <CartModal calculateTotal={calculateTotal} />
