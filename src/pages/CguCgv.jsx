1fef6ec9e789fce655bd33dcac7c81fec840c902
Mehdirps <mehdi.raposo77@gmail.com>
2025-03-23 16:01:23 +0100

    🎨 lience proprietaire

--- src/pages/CguCgv.jsx
+++ src/pages/CguCgv.jsx
@@ -4,94 +4,94 @@ const CguCgv = () => {
     return (
         <div>
             <section id="cgu">
-        <h2>1. Présentation de l'application</h2>
-        <p>L'application Herberginfos est éditée par Herberginfos dont le siège social est situé à 2 rue de Paris</p>
-        <p>Pour toute question ou demande d'information, vous pouvez nous contacter à <a href="mailto:contact@herbeginfos.fr">contact@herbeginfos.fr</a> ou par téléphone au 0645686212.</p>
+                <h2>1. Présentation de l'application</h2>
+                <p>L'application Herberginfos est éditée par Herberginfos dont le siège social est situé à 2 rue de Paris.</p>
+                <p>Pour toute question ou demande d'information, vous pouvez nous contacter à <a href="mailto:contact@herbeginfos.fr">contact@herbeginfos.fr</a> ou par téléphone au 0645686212.</p>
 
-        <h2>2. Acceptation des CGU</h2>
-        <p>En utilisant l'application Herberginfos, l'utilisateur reconnaît avoir lu et accepté les présentes Conditions Générales d'Utilisation.</p>
+                <h2>2. Acceptation des CGU</h2>
+                <p>En utilisant l'application Herberginfos, l'utilisateur reconnaît avoir lu et accepté les présentes Conditions Générales d'Utilisation.</p>
 
-        <h2>3. Description des services</h2>
-        <p>L'application Herberginfos permet aux utilisateurs de créer un livret d'accueil numérique pour les voyageurs séjournant dans leur hébergement.</p>
+                <h2>3. Description des services</h2>
+                <p>L'application Herberginfos permet aux utilisateurs de créer un livret d'accueil numérique pour les voyageurs séjournant dans leur hébergement.</p>
 
-        <h2>4. Accès et utilisation de l'application</h2>
-        <ul>
-            <li>L'accès à l'application est réservé aux utilisateurs ayant créé un compte et accepté les présentes CGU.</li>
-            <li>L'utilisateur s'engage à fournir des informations exactes lors de son inscription et à les maintenir à jour.</li>
-            <li>Toute utilisation frauduleuse ou abusive de l'application entraînera la suspension ou la suppression du compte utilisateur.</li>
-        </ul>
+                <h2>4. Accès et utilisation de l'application</h2>
+                <ul>
+                    <li>L'accès à l'application est réservé aux utilisateurs ayant créé un compte et accepté les présentes CGU.</li>
+                    <li>L'utilisateur s'engage à fournir des informations exactes lors de son inscription et à les maintenir à jour.</li>
+                    <li>Toute utilisation frauduleuse ou abusive de l'application entraînera la suspension ou la suppression du compte utilisateur.</li>
+                </ul>
 
-        <h2>5. Responsabilités</h2>
-        <ul>
-            <li>L'utilisateur est seul responsable des informations qu'il fournit et des actions qu'il effectue sur l'application.</li>
-            <li>Herberginfos ne pourra être tenue responsable en cas de dommages directs ou indirects résultant de l'utilisation de l'application.</li>
-        </ul>
+                <h2>5. Responsabilités</h2>
+                <ul>
+                    <li>L'utilisateur est seul responsable des informations qu'il fournit et des actions qu'il effectue sur l'application.</li>
+                    <li>Herberginfos ne pourra être tenue responsable en cas de dommages directs ou indirects résultant de l'utilisation de l'application.</li>
+                </ul>
 
-        <h2>6. Propriété intellectuelle</h2>
-        <ul>
-            <li>Tous les contenus disponibles sur l'application, y compris les textes, images, logos, et logiciels, sont la propriété exclusive de Herberginfos.</li>
-            <li>Toute reproduction, modification, ou utilisation non autorisée est strictement interdite.</li>
-        </ul>
+                <h2>6. Propriété intellectuelle et licence</h2>
+                <ul>
+                    <li>Tous les contenus disponibles sur l'application, y compris les textes, images, logos, et logiciels, sont la propriété exclusive de Herberginfos.</li>
+                    <li>L'application Herberginfos est distribuée sous licence propriétaire. Toute reproduction, modification, ou utilisation non autorisée de son code source est strictement interdite.</li>
+                </ul>
 
-        <h2>7. Modification des CGU</h2>
-        <p>Herberginfos se réserve le droit de modifier les présentes CGU à tout moment. Les modifications seront notifiées aux utilisateurs par email et prendront effet immédiatement.</p>
+                <h2>7. Modification des CGU</h2>
+                <p>Herberginfos se réserve le droit de modifier les présentes CGU à tout moment. Les modifications seront notifiées aux utilisateurs par email et prendront effet immédiatement.</p>
 
-        <h2>8. Loi applicable et juridiction compétente</h2>
-        <p>Les présentes CGU sont régies par la loi française. Tout litige relatif à leur interprétation ou exécution sera soumis aux tribunaux compétents de Sens.</p>
-    </section>
+                <h2>8. Loi applicable et juridiction compétente</h2>
+                <p>Les présentes CGU sont régies par la loi française. Tout litige relatif à leur interprétation ou exécution sera soumis aux tribunaux compétents de Sens.</p>
+            </section>
 
-    <h1>Conditions Générales de Vente (CGV)</h1>
-    <section id="cgv">
-        <h2>1. Objet</h2>
-        <p>Les présentes Conditions Générales de Vente régissent les relations contractuelles entre Herberginfos et tout client effectuant un achat via l'application Herberginfos.</p>
+            <h1>Conditions Générales de Vente (CGV)</h1>
+            <section id="cgv">
+                <h2>1. Objet</h2>
+                <p>Les présentes Conditions Générales de Vente régissent les relations contractuelles entre Herberginfos et tout client effectuant un achat via l'application Herberginfos.</p>
 
-        <h2>2. Produits et services</h2>
-        <p>Herberginfos propose à la vente des produits et services liés à la création de livrets d'accueil numériques pour les voyageurs.</p>
+                <h2>2. Produits et services</h2>
+                <p>Herberginfos propose à la vente des produits et services liés à la création de livrets d'accueil numériques pour les voyageurs.</p>
 
-        <h2>3. Tarifs</h2>
-        <ul>
-            <li>Les prix des produits et services sont indiqués en euros, toutes taxes comprises (TTC).</li>
-            <li>Tarif de 4.90€ pour la livraison d’une commande.</li>
-        </ul>
+                <h2>3. Tarifs</h2>
+                <ul>
+                    <li>Les prix des produits et services sont indiqués en euros, toutes taxes comprises (TTC).</li>
+                    <li>Tarif de 4.90€ pour la livraison d’une commande.</li>
+                </ul>
 
-        <h2>4. Commandes</h2>
-        <ul>
-            <li>Toute commande effectuée sur l'application implique l'acceptation des présentes CGV.</li>
-            <li>Le client recevra une confirmation de commande par e-mail à l'adresse fournie lors de l'achat.</li>
-        </ul>
+                <h2>4. Commandes</h2>
+                <ul>
+                    <li>Toute commande effectuée sur l'application implique l'acceptation des présentes CGV.</li>
+                    <li>Le client recevra une confirmation de commande par e-mail à l'adresse fournie lors de l'achat.</li>
+                </ul>
 
-        <h2>5. Paiement</h2>
-        <ul>
-            <li>Les paiements peuvent être effectués par carte bancaire via Stripe.</li>
-            <li>Toute commande sera traitée après validation du paiement.</li>
-        </ul>
+                <h2>5. Paiement</h2>
+                <ul>
+                    <li>Les paiements peuvent être effectués par carte bancaire via Stripe.</li>
+                    <li>Toute commande sera traitée après validation du paiement.</li>
+                </ul>
 
-        <h2>6. Rétractation et remboursement</h2>
-        <ul>
-            <li>Conformément à la législation en vigueur, le client dispose d'un délai de rétractation de 14 jours à compter de la réception du produit ou de la commande du service.</li>
-            <li>Pour exercer ce droit, le client doit contacter Herberginfos à <a href="mailto:contact@herbeginfos.fr">contact@herbeginfos.fr</a>.</li>
-        </ul>
+                <h2>6. Rétractation et remboursement</h2>
+                <ul>
+                    <li>Conformément à la législation en vigueur, le client dispose d'un délai de rétractation de 14 jours à compter de la réception du produit ou de la commande du service.</li>
+                    <li>Pour exercer ce droit, le client doit contacter Herberginfos à <a href="mailto:contact@herbeginfos.fr">contact@herbeginfos.fr</a>.</li>
+                </ul>
 
-        <h2>7. Livraison</h2>
-        <ul>
-            <li>Livraison sous 3 à 5 jours ouvrés en France métropolitaine.</li>
-            <li>En cas de retard de livraison, le client sera informé dans les meilleurs délais.</li>
-        </ul>
+                <h2>7. Livraison</h2>
+                <ul>
+                    <li>Livraison sous 3 à 5 jours ouvrés en France métropolitaine.</li>
+                    <li>En cas de retard de livraison, le client sera informé dans les meilleurs délais.</li>
+                </ul>
 
-        <h2>8. Garantie et service après-vente</h2>
-        <p>[Décrivez les garanties offertes et les conditions de service après-vente].</p>
+                <h2>8. Garantie et service après-vente</h2>
+                <p>[Décrivez les garanties offertes et les conditions de service après-vente].</p>
 
-        <h2>9. Responsabilités</h2>
-        <ul>
-            <li>Herberginfos ne pourra être tenue responsable des dommages résultant de l'utilisation des produits ou services vendus.</li>
-        </ul>
+                <h2>9. Responsabilités</h2>
+                <ul>
+                    <li>Herberginfos ne pourra être tenue responsable des dommages résultant de l'utilisation des produits ou services vendus.</li>
+                </ul>
 
-        <h2>10. Modification des CGV</h2>
-        <p>Herberginfos se réserve le droit de modifier les présentes CGV à tout moment. Les modifications seront notifiées aux clients par email et prendront effet immédiatement.</p>
+                <h2>10. Modification des CGV</h2>
+                <p>Herberginfos se réserve le droit de modifier les présentes CGV à tout moment. Les modifications seront notifiées aux clients par email et prendront effet immédiatement.</p>
 
-        <h2>11. Loi applicable et juridiction compétente</h2>
-        <p>Les présentes CGV sont régies par la loi française. Tout litige relatif à leur interprétation ou exécution sera soumis aux tribunaux compétents de [Ville].</p>
-    </section>
+                <h2>11. Loi applicable et juridiction compétente</h2>
+                <p>Les présentes CGV sont régies par la loi française. Tout litige relatif à leur interprétation ou exécution sera soumis aux tribunaux compétents de [Ville].</p>
+            </section>
         </div>
     );
 };
